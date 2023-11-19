2a427db1f3c20d1355b682f8752d440efbfaba17
Fares <faressolibie@gmail.com>
2023-11-19 13:09:39 -0300

    atualizando script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -7,6 +7,6 @@ cd /home/ec2-user/chat
 if sudo pm2 list | grep -q "$PM2_PROCESS_NAME"; then
   sudo pm2 restart "$PM2_PROCESS_NAME" --update-env
 else
-  sudo pm2 start dist/main/index.js --name="$PM2_PROCESS_NAME" 
+  sudo pm2 start build/index.js --name="$PM2_PROCESS_NAME" 
   sudo pm2 save
 fi
