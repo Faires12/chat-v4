52247cfd7c197031b86bba85aa25f0965a49980b
Fares <faressolibie@gmail.com>
2023-11-19 21:54:48 -0300

    mudando script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PM2_PROCESS_NAME="chat"
+PM2_PROCESS_NAME="server"
 
-cd /home/ec2-user/chat
+cd /home/ec2-user/server
 
 if sudo pm2 list | grep -q "$PM2_PROCESS_NAME"; then
   sudo pm2 restart "$PM2_PROCESS_NAME" --update-env
